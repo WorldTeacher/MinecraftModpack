31797aace4f35a372f3ac0d5c383fb36f2577d18
WorldTeacher <41587052+WorldTeacher@users.noreply.github.com>
2023-01-02 12:52:48 +0100

    add update files for win/linux

---
+++ update.bat
@@ -0,0 +1,4 @@
+#create a script that will check the git repo for changes and update the files
+
+#update git repo
+git pull
---
+++ update.sh
@@ -0,0 +1,6 @@
+#run update.bat in shell using PS
+
+#set the path to the directory where the update.bat is located
+
+#pull the latest version of the code from the repository
+git pull
