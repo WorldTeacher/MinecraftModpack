fca60566f70c65e8b4d24bae1f75cee856bf0a0e
WorldTeacher <41587052+WorldTeacher@users.noreply.github.com>
2023-01-05 10:23:35 +0100

    change names

---
+++ update_modpack_windows.bat
@@ -0,0 +1,10 @@
+#create a script that will check the git repo for changes and update the files
+
+#check if git is installed
+
+
+#update git repo
+git pull
+
+:gitnotinstalled:
+echo Git is not installed. Please install git and try again.
